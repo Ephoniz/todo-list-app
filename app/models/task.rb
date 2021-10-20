5bd016f28ecd5931c5ffceb589573b5bb6590b7b
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-10-20 12:23:31 +0200

    Added tasks model

---
+++ app/models/task.rb
@@ -0,0 +1,9 @@
+class Task < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :status, presence: true, inclusion: { in: %w[to_do done],
+                                                  message: "#{value} is not a valid status" }
+  validates :imporatnce, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5],
+                                                      message: "#{value} is not a valid level of importance" }
+end
