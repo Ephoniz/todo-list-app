dc36944961a3801a6ff54b73d0b4c037be0692ad
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-10-20 12:39:16 +0200

    Added devise views and tasks controller

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,36 @@
+class TaskController < ApplicationController
+  def index
+    @tasks = current_user.tasks
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @tasks, notice: "Flat was successfully created." }
+        format.json { render :show, status: :created, location: @flat }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+end
